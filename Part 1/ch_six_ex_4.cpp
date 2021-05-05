22fc6c02a907cd5c124f8e0f7e509eac236a810e
fl4tley <jhn.tly@gmail.com>
2021-05-04 20:07:28 -0400

    updated exercise 4

--- Part 1/ch_six_ex_4.cpp
+++ Part 1/ch_six_ex_4.cpp
@@ -1,3 +1,6 @@
+// This solution error checks the input but does not error if there
+// is a duplicate, it only prints out that there was a duplicate.
+
 #include "../std_lib_facilities.h"
 
 class Name_value{
@@ -6,7 +9,6 @@ class Name_value{
         bool is_equal(Name_value comp);
         string name;
         int age;
-
 };
 
 bool Name_value::is_equal(Name_value comp){
@@ -20,14 +22,13 @@ int main(){
 
     vector<Name_value> nv_list;
     bool repeat = false;
-
+    string new_name; 
+    int new_age;
+    
     while(true){
-        cout << "Enter a name value pair: \n";
-        
-        string new_name; int new_age;
-        
-        cin >> new_name >> new_age;
-        if(new_name == "NoName" && new_age == 0) break;
+        if (cout << "Enter a name value pair: " && !(cin >> new_name >> new_age)) error("Invalid format detected. Please use 'string int' format."); 
+        if (new_name == "NoName" && new_age == 0) break;
+
         Name_value new_nv = Name_value(new_name, new_age); 
         
         for (Name_value nv : nv_list){ // could use a map for more speed I think 
@@ -36,9 +37,11 @@ int main(){
 
         nv_list.push_back(new_nv);    
     }
+
+    cout << "------------------\nprinting names\n------------------\n"; 
     for(Name_value nv : nv_list){
         cout << nv.name << " " << nv.age << endl; 
     }
-    if (repeat) error("Repeated a name.");
+    if (repeat) cout << "\nWARNING: Repeated a name.";
     return 0;
 }
